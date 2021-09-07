14c28e9b876779dc8c09c24e060aacf347ff9443
Kha-Dinh Luong <kvl11@case.edu>
2021-09-06 19:56:20 -0700

    Update shared/non-shared weight options for SAMPN.

--- SAMPNN.py
+++ SAMPNN.py
@@ -14,6 +14,10 @@ class SAMPNConv(nn.Module):
         self.input_nn = input_nn
         self.hidden_nn = hidden_nn
 
+    def reset_parameters(self):
+        self.input_nn.reset_parameters()
+        self.hidden_nn.reset_parameters()
+
     def forward(self, g, node_feats, edge_feats, messages):
         with g.local_scope():
             g.edata['pre_mess'] = messages
@@ -41,32 +45,66 @@ class SAMPNGNN(nn.Module):
                  node_out_feats,
                  edge_in_feats,
                  message_hidden_feats,
-                 num_message_passing=6):
+                 num_message_passing=6,
+                 shared_message_passing_weights=True):
         super(SAMPNGNN, self).__init__()
 
         self.num_message_passing = num_message_passing
 
-        self.W_in = nn.Linear(node_in_feats+edge_in_feats,message_hidden_feats)
+        self.shared_message_passing_weights = shared_message_passing_weights
 
-        self.W_h = nn.Linear(message_hidden_feats,message_hidden_feats)
+        if self.shared_message_passing_weights:
+            self.W_in = nn.Linear(node_in_feats+edge_in_feats,message_hidden_feats)
+
+            self.W_h = nn.Linear(message_hidden_feats,message_hidden_feats)
+
+            self.gnn_layer = SAMPNConv(message_hidden_feats,self.W_in,self.W_h)
+        else:
+            W_in = nn.Linear(node_in_feats+edge_in_feats,message_hidden_feats)
+
+            W_h = nn.Linear(message_hidden_feats,message_hidden_feats)
+
+            gnn_layer = SAMPNConv(message_hidden_feats,W_in,W_h)
+
+            self.first_W_in = W_in
+
+            self.gnn_layers = nn.ModuleList({gnn_layer})
+
+            for _ in range(num_message_passing-1):
+                W_in = nn.Linear(message_hidden_feats,message_hidden_feats)
+
+                W_h = nn.Linear(message_hidden_feats,message_hidden_feats)
+
+                gnn_layer = SAMPNConv(message_hidden_feats,W_in,W_h)
+
+                self.gnn_layers.append(gnn_layer)
 
         self.W_ah = nn.Linear(node_in_feats,message_hidden_feats)
 
         self.W_o = nn.Linear(message_hidden_feats,node_out_feats)
 
-        self.gnn_layer = SAMPNConv(message_hidden_feats,self.W_in,self.W_h)
-
     def reset_parameters(self):
-        pass
+        self.W_ah.reset_parameters()
+        self.W_o.reset_parameters()
+        if self.shared_message_passing_weights:
+            self.gnn_layer.reset_parameters()
+        else:
+            for layer in self.gnn_layers:
+                layer.reset_parameters()
     
     def forward(self, g, node_feats, edge_feats):
         g.ndata['node_feats'] = node_feats
         g.apply_edges(lambda edges:{'src_feats':edges.src['node_feats']})
         src_feats = g.edata.pop('src_feats')
         g.ndata.pop('node_feats')
-        messages = torch.relu(self.W_in(torch.cat((src_feats,edge_feats),dim=1)))
-        for _ in range(self.num_message_passing):
-            messages = self.gnn_layer(g, node_feats, edge_feats, messages)
+        if self.shared_message_passing_weights:
+            messages = torch.relu(self.W_in(torch.cat((src_feats,edge_feats),dim=1)))
+            for _ in range(self.num_message_passing):
+                messages = self.gnn_layer(g, node_feats, edge_feats, messages)
+        else:
+            messages = torch.relu(self.first_W_in(torch.cat((src_feats,edge_feats),dim=1)))
+            for _ in range(self.num_message_passing):
+                messages = self.gnn_layers[_](g, node_feats, edge_feats, messages)
         g.edata['message'] = messages
         g.update_all(fn.copy_e('message','s'),
                      fn.sum('s','neighbor_sum'))
@@ -87,14 +125,16 @@ class SAMPNPredictor(nn.Module):
                  node_out_feats=64,
                  num_tasks=1,
                  num_message_passing=6,
-                 drop_out_rate=0):
+                 drop_out_rate=0,
+                 shared_message_passing_weights=True):
         super(SAMPNPredictor, self).__init__()
 
         self.gnn = SAMPNGNN(node_in_feats=node_in_feats,
                             node_out_feats=node_out_feats,
                             edge_in_feats=edge_in_feats,
                             message_hidden_feats=message_hidden_feats,
-                            num_message_passing=num_message_passing)
+                            num_message_passing=num_message_passing,
+                            shared_message_passing_weights=shared_message_passing_weights)
         
         self.readout = AvgPooling()
 
