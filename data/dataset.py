75ecc72834905555995d36d42e1a9e4d45c1eff3
lvkd84 <32368540+lvkd84@users.noreply.github.com>
2021-09-13 22:53:31 -0700

    Add some dataloader functions

--- data/dataset.py
+++ data/dataset.py
@@ -90,20 +90,111 @@ class MolecularDataLoader(Iterable):
         except StopIteration:
             raise StopIteration
 
-def get_LIPO_dataset():
-    pass
+import os
+CURRENT_FOLDER = os.path.abspath(os.getcwd())
 
-def get_SIDER_dataset():
-    pass
+LIPO_PATH = CURRENT_FOLDER + '/' + 'Lipophilicity.csv'
+def get_LIPO_dataloader(node_featurizer=CanonicalAtomFeaturizer,
+                        edge_featurizer=CanonicalBondFeaturizer,
+                        batch_size=8,
+                        shuffle=True):
 
-def get_BBBP_dataset():
-    pass
+    return MolecularDataLoader(data_path=LIPO_PATH,
+                                task_names='exp',
+                                smile_column='smiles',
+                                node_featurizer=node_featurizer,
+                                edge_featurizer=edge_featurizer,
+                                batch_size=batch_size,
+                                shuffle=shuffle)
+
+SIDER_PATH = CURRENT_FOLDER + '/' + 'sider.csv'
+sider_tasks = ["Hepatobiliary disorders",
+                "Metabolism and nutrition disorders",
+                "Product issues",
+                "Eye disorders",
+                "Investigations",
+                "Musculoskeletal and connective tissue disorders",
+                "Gastrointestinal disorders",
+                "Social circumstances",
+                "Immune system disorders",
+                "Reproductive system and breast disorders",
+                "Neoplasms benign, malignant and unspecified (incl cysts and polyps)",
+                "General disorders and administration site conditions",
+                "Endocrine disorders",
+                "Surgical and medical procedures",
+                "Vascular disorders",
+                "Blood and lymphatic system disorders",
+                "Skin and subcutaneous tissue disorders",
+                "Congenital, familial and genetic disorders",
+                "Infections and infestations",
+                "Respiratory, thoracic and mediastinal disorders",
+                "Psychiatric disorders",
+                "Renal and urinary disorders",
+                "Pregnancy, puerperium and perinatal conditions",
+                "Ear and labyrinth disorders",
+                "Cardiac disorders",
+                "Nervous system disorders",
+                "Injury, poisoning and procedural complications"]
+def get_SIDER_dataloader(node_featurizer=CanonicalAtomFeaturizer,
+                        edge_featurizer=CanonicalBondFeaturizer,
+                        batch_size=8,
+                        shuffle=True):
+
+    return MolecularDataLoader(data_path=SIDER_PATH,
+                            task_names=sider_tasks,
+                            smile_column='smiles',
+                            node_featurizer=node_featurizer,
+                            edge_featurizer=edge_featurizer,
+                            batch_size=batch_size,
+                            shuffle=shuffle)
+
+BBBP_PATH = CURRENT_FOLDER + '/' + 'BBBP.csv'
+def get_BBBP_dataloader(node_featurizer=CanonicalAtomFeaturizer,
+                        edge_featurizer=CanonicalBondFeaturizer,
+                        batch_size=8,
+                        shuffle=True):
+    
+    return MolecularDataLoader(data_path=SIDER_PATH,
+                            task_names='p_np',
+                            smile_column='smiles',
+                            node_featurizer=node_featurizer,
+                            edge_featurizer=edge_featurizer,
+                            batch_size=batch_size,
+                            shuffle=shuffle)
 
 def get_HIV_dataset():
     pass
 
-def get_MUV_dataset():
-    pass
+MUV_PATH = CURRENT_FOLDER + '/' + 'muv.csv'
+muv_tasks = ['MUV-466',
+            'MUV-548',
+            'MUV-600',
+            'MUV-644',
+            'MUV-652',
+            'MUV-689',
+            'MUV-692',
+            'MUV-712',
+            'MUV-713',
+            'MUV-733',
+            'MUV-737',
+            'MUV-810',
+            'MUV-832',
+            'MUV-846',
+            'MUV-852',
+            'MUV-858',
+            'MUV-859']
+def get_MUV_dataloader(node_featurizer=CanonicalAtomFeaturizer,
+                        edge_featurizer=CanonicalBondFeaturizer,
+                        batch_size=8,
+                        shuffle=True):
+
+    return MolecularDataLoader(data_path=MUV_PATH,
+                            task_names=muv_tasks,
+                            smile_column='smiles',
+                            node_featurizer=node_featurizer,
+                            edge_featurizer=edge_featurizer,
+                            batch_size=batch_size,
+                            shuffle=shuffle)
 
 def get_ESOL_dataset():
     pass
