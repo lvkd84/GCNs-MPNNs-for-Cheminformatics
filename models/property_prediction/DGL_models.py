94057d4bdf8b9f6bbc8d97ac83d914deb723e258
lvkd84 <32368540+lvkd84@users.noreply.github.com>
2021-09-23 08:10:06 -0700

    Include DGL-Life model zoo.

---
+++ models/property_prediction/DGL_models.py
@@ -0,0 +1,6 @@
+from dgllife.model.model_zoo.gat_predictor import GATPredictor
+from dgllife.model.model_zoo.gcn_predictor import GCNPredictor
+from dgllife.model.model_zoo.mgcn_predictor import MGCNPredictor
+from dgllife.model.model_zoo.mpnn_predictor import MPNNPredictor
+from dgllife.model.model_zoo.schnet_predictor import SchNetPredictor
+from dgllife.model.model_zoo.weave_predictor import WeavePredictor
--- models/property_prediction/__init__.py
+++ models/property_prediction/__init__.py
@@ -3,3 +3,4 @@ from .DMPNN import DMPNNPredictor
 from .SAMPNN import SAMPNPredictor
 from .EMNN import EMNNPredictor
 from .DimeNet import DimePredictor
+from .DGL_models import *
